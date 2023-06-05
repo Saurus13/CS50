637b1740babacaf05ca6c8c183b881283fbf8db2
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-06-05 20:52:15 +0000

    Mon, Jun 5, 2023, 4:52 PM -04:00

---
+++ population/population.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("Start Size: ");
+    }
+    while (n < 9);
+
+    // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End Size: ");
+    }
+    while (end < start);
+
+    // TODO: Calculate number of years until we reach threshold
+    for(int i = 0; i == end; i++){
+
+    }
+    // TODO: Print number of years
+}

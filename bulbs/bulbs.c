02be19fe0cf3cd45611023e928721e2800d52afe
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-06-28 19:42:15 +0000

    Wed, Jun 28, 2023, 3:42 PM -04:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -10,6 +10,7 @@ void print_bulb(int bit);
 int main(void)
 {
     string message = get_string("Message: ");
+    printf("%i\n", strlen(message));
     int bits[8];
     for (int i = 0; i < strlen(message); i++) //loop through string
     {

7d4fc6892d2a0e94b9ea84228719eea270430af3
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-06-28 19:20:39 +0000

    Wed, Jun 28, 2023, 3:20 PM -04:00

--- hello.c
+++ hello.c
@@ -5,5 +5,5 @@ int main(void)
 {
     //string name = get_string("What is your name? "); //ask for user's name
     //printf("hello, %s\n", name); //print name
-    printf("%i", 'c'/2);
+    printf("%i", ' '/2);
 }

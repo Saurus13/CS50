3b32e8d579c3605761ac96140af482ac666453f7
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-13 21:22:54 +0000

    Thu, Jul 13, 2023, 5:22 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -245,7 +245,10 @@ void eliminate(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (candidates[i].votes == min && candidates[i].eliminated == false)
+        {
+            candidates[i].eliminated = true;
+        }
     }
     return;
 }

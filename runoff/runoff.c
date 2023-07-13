4751a0d2823575317d6d90a1946473b09e638ea1
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-13 18:58:17 +0000

    Thu, Jul 13, 2023, 2:58 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -192,10 +192,7 @@ int find_min(void)
         }
         else
         {
-            if (candidates[i+1].votes < min_num)
-            {
-                min_num = candidates[i+1].votes;
-            }
+            min_num = candidates[i+1].votes;
         }
     }
     return min_num;

9ec068bd53363da4ace3e318b0ed2aac006018d1
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-13 19:01:09 +0000

    Thu, Jul 13, 2023, 3:01 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -183,14 +183,11 @@ int find_min(void)
     int min_num = candidates[0].votes;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false)
+        if (candidates[i].eliminated == false && candidates[i].votes < min_num)
         {
-            if (candidates[i].votes < min_num)
-            {
-                min_num = candidates[i].votes;
-            }
+            min_num = candidates[i].votes;
         }
-        else
+        else if (candidates[i].eliminated == true)
         {
             min_num = candidates[i+1].votes;
         }

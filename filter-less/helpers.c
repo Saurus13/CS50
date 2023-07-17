d13308e78fce9e99d09e4aa0243b57a33177ede6
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-17 21:28:49 +0000

    Mon, Jul 17, 2023, 5:28 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -60,7 +60,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-
+            image
+            swap (image[i][j] & image[i][width-j])
         }
     }
     return;

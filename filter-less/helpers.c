4a35dc2692293eb66c5b53a6cacc3b48dbbce834
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-18 19:13:23 +0000

    Tue, Jul 18, 2023, 3:13 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -80,11 +80,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-
+            copy[i][j].rgbtRed = image[i][j].rgbtRed;
+            copy[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            copy[i][j].rgbtGreen = image[i][j].rgbtGreen;
         }
     }
     return;

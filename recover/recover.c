92db11c93c0a6b2c49cd33fa9f87e184f6c018df
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-19 20:06:41 +0000

    Wed, Jul 19, 2023, 4:06 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover image\n");
+        printf("Usage: ./recover IMAGE\n");
         return 1;
     }
     return 0;

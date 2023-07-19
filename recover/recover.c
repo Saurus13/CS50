97d6da193f9061ad5df5036508298de5468d9a84
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-19 20:08:38 +0000

    Wed, Jul 19, 2023, 4:08 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,10 +6,13 @@
 #define SIZE 512;
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 2) //check if user runs program in valid manner
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
+    
+
+
     return 0;
 }

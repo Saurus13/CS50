8907eddd7d8e42ad47b66f71ae8a62f51754bf64
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-27 21:24:58 +0000

    Thu, Jul 27, 2023, 5:24 PM -04:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,14 @@
-# TODO
+from cs50 import get_int
+
+while True:
+    num = get_int("Height: ")
+    if num > 0 and num < 9:
+        break
+
+for i in range (0, num, 1):
+    for j in range (0, num, 1):
+        if (i + j < num - 1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()

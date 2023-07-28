1f5cbd9527b1b56cb6179d0dc8d8f735105db779
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-28 14:17:28 +0000

    Fri, Jul 28, 2023, 10:17 AM -04:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,7 +1,17 @@
 from cs50 import get_float
 
-while True:  # prompt user for money
-    num = get_float("Change owed: ")
-    if num > 0:
-        break
+
+def main():
+    cents = get_cents()
+    quarters = calculate_quarters(cents)
+    dimes = calculate_dimes(cents)
+    nickels = calculate_nickels(cents)
+    pennies = calculate_pennies(cents)
+
+
+def get_cents():
+    while True:  # prompt user for money
+        num = get_float("Change owed: ")
+        if num > 0:
+            return num
 

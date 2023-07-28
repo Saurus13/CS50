079db447f15c60761a771404a0da3fa7e9ae56fe
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-28 18:07:53 +0000

    Fri, Jul 28, 2023, 2:07 PM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,2 +1,20 @@
 from cs50 import get_string
 
+def main():
+    string = get_string("Text: ")
+    L = 100.0 * count_letters(string) / count_words(string)
+    S = 100.0 * count_sentences(string) / count_words(string)
+    index = 0.0588 * L - 0.296 * S - 15.8; # Coleman-Liau Formula
+
+    if index < 1.0:
+        printf("Before Grade 1\n")
+
+    elif index > 16.0:
+        printf("Grade 16+\n")
+
+    elif 1.0 < index < 16.0:
+        printf("Grade %i\n", rounded_index)
+
+def count_letters(string):
+    
+main()

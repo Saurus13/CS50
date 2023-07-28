008e88913ffa83144752a9c6304afe13b5390105
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-28 23:44:38 +0000

    Fri, Jul 28, 2023, 7:44 PM -04:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,8 +5,8 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argv) != 3:
-        sys.exit("Usage: python tournament.py FILENAME")
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python dna.py data.csv sequence.txt")
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable

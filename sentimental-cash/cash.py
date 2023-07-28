b0f3cabe54d5e33d1ff295abfa6b7ccf582d6397
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-07-28 17:34:26 +0000

    Fri, Jul 28, 2023, 1:34 PM -04:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -19,11 +19,13 @@ def main():
     coins = quarters + dimes + nickels + pennies  # calculate total # of coins
     print(f"{coins}")
 
+
 def get_cents():
     while True:  # prompt user for money
         num = get_float("Change owed: ")
         if num > 0:
             return num
+
 
 def calculate_quarters(cents):  # find how many quarters required
     quarters = 0
@@ -32,12 +34,14 @@ def calculate_quarters(cents):  # find how many quarters required
             quarters += 1
     return quarters
 
+
 def calculate_dimes(cents):  # find how many dimes required
     dimes = 0
     for i in range(1, cents + 1, 1):
         if i % 10 == 0:
             dimes += 1
     return dimes
+
 
 def calculate_nickels(cents):  # find how many nickels required
     nickels = 0
@@ -46,6 +50,7 @@ def calculate_nickels(cents):  # find how many nickels required
             nickels += 1
     return nickels
 
+
 def calculate_pennies(cents):  # find how many pennies required
     pennies = 0
     for i in range(1, cents + 1, 1):
@@ -53,6 +58,5 @@ def calculate_pennies(cents):  # find how many pennies required
             pennies += 1
     return pennies
 
+
 main()
-
-

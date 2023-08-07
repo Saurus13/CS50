54d4fe09fb7edb6d393f1f17c6ad2667f883436b
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-08-07 13:56:31 +0000

    Mon, Aug 7, 2023, 9:56 AM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT year FROM crime_scene_reports;
+SELECT month, day FROM crime_scene_reports WHERE month = 07 AND day = 28;

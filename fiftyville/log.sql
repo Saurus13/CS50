e1b2d427c1f61287dcf44df5a0ebbc73a30eecdb
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-08-07 19:27:46 +0000

    Mon, Aug 7, 2023, 3:27 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,3 +4,9 @@ SELECT month, day FROM crime_scene_reports WHERE month = 07 AND day = 28;
 SELECT month, day, street FROM crime_scene_reports WHERE month = 07 AND day = 28 AND street = "Humphrey Street";
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28;
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
+
+SELECT people.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_transactions.year = 2021 AND atm_transactions.month = 7 AND atm_transactions.day = 28
+AND atm_location = "Leggett Street" AND atm_transactions.transaction_type = "withdraw";

ccfaa48214c30521a7ba20c9860032113ea0d965
Saurus13 <71090029+Saurus13@users.noreply.github.com>
2023-08-08 19:29:22 +0000

    Tue, Aug 8, 2023, 3:29 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -22,3 +22,14 @@ WHERE flights.destination_airport_id = airports.id;
 SELECT id, hour, minute, origin_airport_id, destination_airport_id FROM flights WHERE year = 2021
 AND month = 7 AND day = 29 ORDER BY hour ASC;
 
+SELECT name FROM people JOIN passengers ON people.passport_number = passengers.passport_number
+JOIN flights ON flights.id = passengers.flight_id WHERE (flights.year = 2021 AND flights.month = 7 AND flights.day = 29 AND flights.id = 36)
+AND name IN (SELECT phone_calls.caller_name FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60)
+AND name IN (SELECT people.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_transactions.year = 2021 AND atm_transactions.month = 7 AND atm_transactions.day = 28
+AND atm_location = "Leggett Street" AND atm_transactions.transaction_type = "withdraw")
+AND name IN (JOIN bakery_security_logs ON bakery_security_logs.licence_plate = people.licence_plate
+WHERE bakery_security_logs.year = 2021 AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10 AND bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <=25);
